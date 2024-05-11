3b5224c08e260917f4661746bc0041525a533214
Abhay Tiwari <abhaytiwariwork@gmail.com>
2024-05-11 19:05:53 +0530

    updated skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,134 +1,134 @@
 // Formspree code
-const form = document.getElementById("contact-form");
+const form = document.getElementById('contact-form');
 
 async function handleSubmit(event) {
   event.preventDefault();
-  var status = document.getElementById("alert");
+  var status = document.getElementById('alert');
   var data = new FormData(event.target);
   fetch(event.target.action, {
     method: form.method,
     body: data,
     headers: {
-      Accept: "application/json",
+      Accept: 'application/json',
     },
   })
     .then((response) => {
-      status.innerHTML = "Your message has been sent.";
-      document.querySelector(".alert_style").style.display = "block";
+      status.innerHTML = 'Your message has been sent.';
+      document.querySelector('.alert_style').style.display = 'block';
 
       // hide alert after 3 seconds
       setTimeout(function () {
-        document.querySelector(".alert_style").style.display = "none";
+        document.querySelector('.alert_style').style.display = 'none';
       }, 4000);
       form.reset();
     })
     .catch((error) => {
       status.innerHTML =
-        "Oops! There was a problem delivering your message, please contact via other means.";
-      document.querySelector(".alert_style").style.display = "block";
+        'Oops! There was a problem delivering your message, please contact via other means.';
+      document.querySelector('.alert_style').style.display = 'block';
 
       // hide alert after 3 seconds
       setTimeout(function () {
-        document.querySelector(".alert_style").style.display = "none";
+        document.querySelector('.alert_style').style.display = 'none';
       }, 4000);
     });
 }
-form.addEventListener("submit", handleSubmit);
+form.addEventListener('submit', handleSubmit);
 
-// FORM BORDERS 
-$("#contact-form input,#contact-form textarea").on("input focusin",(e)=>{
-  $(e.target).parent().addClass("focusIn");
+// FORM BORDERS
+$('#contact-form input,#contact-form textarea').on('input focusin', (e) => {
+  $(e.target).parent().addClass('focusIn');
   if ($(e.target).val().trim().length > 0) {
-    $(e.target).parent().addClass("valid");
-    $(e.target).parent().removeClass("invalid");
+    $(e.target).parent().addClass('valid');
+    $(e.target).parent().removeClass('invalid');
   } else {
-    $(e.target).parent().addClass("invalid");
-    $(e.target).parent().removeClass("valid");
+    $(e.target).parent().addClass('invalid');
+    $(e.target).parent().removeClass('valid');
   }
 });
 
-$("#contact-form input,#contact-form textarea").on("focusout",(e)=>{
-    $(e.target).parent().removeClass("focusIn");
+$('#contact-form input,#contact-form textarea').on('focusout', (e) => {
+  $(e.target).parent().removeClass('focusIn');
 });
 
 // NAVIGATION PANEL
-let navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+let navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
 
 // MENU SHOW
 if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
   });
 }
 
 // MENU HIDDEN
 if (navClose) {
-  navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
   });
 }
 
 // REMOVE MENU MOBILE
-const navLink = document.querySelectorAll(".nav_link");
+const navLink = document.querySelectorAll('.nav_link');
 
 function linkAction() {
-  navMenu = document.getElementById("nav-menu");
-  navMenu.classList.remove("show-menu");
+  navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show-menu');
 }
-navLink.forEach((n) => n.addEventListener("click", linkAction));
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 // SKILLS
-const skillContent = document.querySelectorAll(".skill");
-const skillHeader = document.querySelectorAll(".skills_header");
+const skillContent = document.querySelectorAll('.skill');
+const skillHeader = document.querySelectorAll('.skills_header');
 const skillContentArr = Array.from(skillContent);
 const skillHeaderArr = Array.from(skillHeader);
 
 skillHeaderArr.forEach((element, idx) => {
-  element.addEventListener("click", function () {
-    skillContentArr[idx].classList.toggle("skills_open");
+  element.addEventListener('click', function () {
+    skillContentArr[idx].classList.toggle('skills_open');
   });
 });
 
 // QUALIFICATION TABS
-let education = document.getElementById("education");
-let work = document.getElementById("work");
-let educationheader = document.getElementById("educationheader");
-let workheader = document.getElementById("workheader");
-workheader.style.color = "var(--text-color)";
-educationheader.style.color = "var(--first-color)";
+let education = document.getElementById('education');
+let work = document.getElementById('work');
+let educationheader = document.getElementById('educationheader');
+let workheader = document.getElementById('workheader');
+workheader.style.color = 'var(--text-color)';
+educationheader.style.color = 'var(--first-color)';
 
-educationheader.addEventListener("click", () => {
-  let condition1 = work.classList.contains("qualification-inactive");
+educationheader.addEventListener('click', () => {
+  let condition1 = work.classList.contains('qualification-inactive');
   if (!condition1) {
-    education.classList.remove("qualification-inactive");
-    work.classList.add("qualification-inactive");
-    workheader.style.color = "var(--text-color)";
-    educationheader.style.color = "var(--first-color)";
+    education.classList.remove('qualification-inactive');
+    work.classList.add('qualification-inactive');
+    workheader.style.color = 'var(--text-color)';
+    educationheader.style.color = 'var(--first-color)';
   }
 });
-workheader.addEventListener("click", () => {
-  let condition2 = education.classList.contains("qualification-inactive");
+workheader.addEventListener('click', () => {
+  let condition2 = education.classList.contains('qualification-inactive');
   if (!condition2) {
-    work.classList.remove("qualification-inactive");
-    education.classList.add("qualification-inactive");
-    educationheader.style.color = "var(--text-color)";
-    workheader.style.color = "var(--first-color)";
+    work.classList.remove('qualification-inactive');
+    education.classList.add('qualification-inactive');
+    educationheader.style.color = 'var(--text-color)';
+    workheader.style.color = 'var(--first-color)';
   }
 });
 
 // PORTFOLIO SWIPER
-let swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper('.mySwiper', {
   cssMode: true,
   loop: true,
 
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
   },
   mousewheel: true,
@@ -136,7 +136,7 @@ let swiper = new Swiper(".mySwiper", {
 });
 
 // SCROLL SECTIONS ACTIVE LINK
-const sections = document.querySelectorAll("section[id]");
+const sections = document.querySelectorAll('section[id]');
 
 function scrollActive() {
   const scrollY = window.pageYOffset;
@@ -144,74 +144,74 @@ function scrollActive() {
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
-    let sectionId = current.getAttribute("id");
+    let sectionId = current.getAttribute('id');
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document
-        .querySelector(".nav_menu a[href*=" + sectionId + "]")
-        .classList.add("active-link");
+        .querySelector('.nav_menu a[href*=' + sectionId + ']')
+        .classList.add('active-link');
     } else {
       document
-        .querySelector(".nav_menu a[href*=" + sectionId + "]")
-        .classList.remove("active-link");
+        .querySelector('.nav_menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link');
     }
   });
 }
-window.addEventListener("scroll", scrollActive);
+window.addEventListener('scroll', scrollActive);
 
 // HEADER SHADOW
 function scrollHeader() {
-  const nav = document.getElementById("header");
-  if (this.scrollY >= 80) nav.classList.add("scroll-header");
-  else nav.classList.remove("scroll-header");
+  const nav = document.getElementById('header');
+  if (this.scrollY >= 80) nav.classList.add('scroll-header');
+  else nav.classList.remove('scroll-header');
 }
-window.addEventListener("scroll", scrollHeader);
+window.addEventListener('scroll', scrollHeader);
 
 // SHOW SCROLL UP BUTTON
 function scrollUpfunc() {
-  const scrollUp = document.getElementById("scroll-up");
-  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
-  else scrollUp.classList.remove("show-scroll");
+  const scrollUp = document.getElementById('scroll-up');
+  if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+  else scrollUp.classList.remove('show-scroll');
 }
-window.addEventListener("scroll", scrollUpfunc);
+window.addEventListener('scroll', scrollUpfunc);
 
 // DARK/LIGHT THEME
-const themeButton = document.getElementById("theme-button");
-const darkTheme = "dark-theme";
-const iconTheme = "uil-sun";
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
 
 // obtain the current theme
 const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme) ? "dark" : "light";
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light';
 const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+  themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
 
 if (selectedTheme) {
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
     darkTheme
   );
-  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](
     iconTheme
   );
 }
 
 // Activate/Deactivate the theme manually with the button
-themeButton.addEventListener("click", () => {
+themeButton.addEventListener('click', () => {
   // Add or remove the dark icon/theme
   document.body.classList.toggle(darkTheme);
   themeButton.classList.toggle(iconTheme);
   // We save the theme and the current icon that the user chose
-  localStorage.setItem("selected-theme", getCurrentTheme());
-  localStorage.setItem("selected-icon", getCurrentIcon());
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
 });
 
 // Typing Animation using Typed JS
-var typed = new Typed(".type", {
-  strings: ["a MERN Stack"],
+var typed = new Typed('.type', {
+  strings: ['a Software', 'a Full Stack'],
   smartBackspace: true,
   startDelay: 900,
   typeSpeed: 130,
